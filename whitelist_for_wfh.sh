9d102863126b76c0a54a2c6f7d4296b5043f0c92
Adam <35268982+AdamHawtin@users.noreply.github.com>
2020-07-31 12:18:59 +0100

    Copy in scripts from toolbox (#1)
    Remove unused concourse bits

---
+++ whitelist_for_wfh.sh
@@ -0,0 +1,33 @@
+if [[ -z "$1" ]]; then
+  echo "Missing required argument: WFH IP Address"
+  exit 1
+fi
+if [[ -z "$2" ]]; then
+  echo "Missing required argument: WFH Person's Name"
+  exit 1
+fi
+WFH_IP=$1
+WFH_NAME=$2
+pushd "${0%/*}" || exit 1
+
+CURRENT_PROJECT=$(gcloud config get-value project 2> /dev/null)
+
+echo "Whitelisting IP: ${WFH_IP} with name ${WFH_NAME} WFH"
+gcloud config set project census-rm-whitelodge
+gcloud container clusters get-credentials rm-k8s-cluster --region europe-west2 --project census-rm-whitelodge
+pipenv run python -m whitelist_service_ip $WFH_IP ops || exit 1
+pipenv run python -m whitelist_service_ip $WFH_IP rabbitmqmanagement || exit 1
+pipenv run python -m whitelist_service_ip $WFH_IP case-api-test || exit 1
+pipenv run python -m whitelist_db_ip $WFH_IP "$WFH_NAME" census-rm-whitelodge
+
+gcloud config set project census-rm-blacklodge
+gcloud container clusters get-credentials rm-k8s-cluster --region europe-west2 --project census-rm-blacklodge
+pipenv run python -m whitelist_service_ip $WFH_IP ops || exit 1
+pipenv run python -m whitelist_service_ip $WFH_IP rabbitmqmanagement || exit 1
+pipenv run python -m whitelist_service_ip $WFH_IP case-api-test || exit 1
+pipenv run python -m whitelist_db_ip $WFH_IP "$WFH_NAME" census-rm-blacklodge
+
+gcloud config set project $CURRENT_PROJECT
+gcloud container clusters get-credentials rm-k8s-cluster --region europe-west2 --project $CURRENT_PROJECT
+
+popd || exit
